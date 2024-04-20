4f91e2e743b2ad3c9190a1deafe5e85c446dd798
grkephart <gary_kephart@pobox.com>
2024-04-20 11:32:14 -0700

    Added AbstractSErvice for non-CRUD services

---
+++ src/main/java/org/drdeesw/commons/services/impl/AbstractService.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package org.drdeesw.commons.services.impl;
+
+import org.drdeesw.commons.services.BusinessService;
+
+/**
+ * 
+ */
+public abstract class AbstractService implements BusinessService
+{
+
+}
